9178d2f67f1d0ee82720a43a2fa97ae41b7ed1af
Pato Perez <pato12p@gmail.com>
2021-01-03 16:56:51 -0300

    improve splash

--- views/splashscreen.tsx
+++ views/splashscreen.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Image, View, ActivityIndicator } from 'react-native';
+
 function SplashScreenView() {
-  return null;
+  return (
+    <React.Fragment>
+      <View style={{ flex: 1, backgroundColor: '#fff', display: 'flex', alignItems: 'center' }}>
+        <Image
+          style={{ width: 150, height: 150, margin: 150 }}
+          source={require('../assets/adaptive-icon.png')}
+        />
+        <ActivityIndicator color="#000" size="large" />
+      </View>
+    </React.Fragment>
+  );
 }
 
 export default SplashScreenView;
